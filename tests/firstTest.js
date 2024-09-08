c75e67528433ce956f2728a2ad57bb207fc24b28
renzenzense <rnzlrn0421@gmail.com>
2024-09-09 01:34:02 +0800

    using assertion to check if the program is behaving as expected

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,17 +1,25 @@
-const {Builder, By, Key} = require ("selenium-webdriver");
+const {Builder, By, Key} = require("selenium-webdriver");
+const assert = require("assert");
 
 async function example(){
     // launch browser
     let driver = await new Builder().forBrowser("chrome").build();
 
     // navigate to application
-    await driver.get("https://lambdatest.github.io/sample-todo-app/")
+    await driver.get("https://lambdatest.github.io/sample-todo-app/");
 
     // add a todo
     await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
 
+    // assert
+    let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+        return value
+    });
+
+    assert.strictEqual(todoText, "Learn Javascript");
+
     // close the browser (best practice)
-    await driver.quit();
+    // await driver.quit();
 }
 
 example()
