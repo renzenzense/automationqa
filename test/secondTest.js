ff243a64fea3d23b677900132cb3ae14b8ff4587
renzenzense <rnzlrn0421@gmail.com>
2024-09-27 11:23:20 +0800

    added mochawesome reports + added mocha flag

--- test/secondTest.js
+++ test/secondTest.js
@@ -24,27 +24,26 @@ describe("add another todo test", function () {
             throw error;  // Ensure Mocha catches the failure
         }
     });
+    // it("should fail because of incorrect text assertion", async function () {
+    //     const chai = await import("chai");
+    //     const should = chai.should();
 
-    it("should fail because of incorrect text assertion", async function () {
-        const chai = await import("chai");
-        const should = chai.should();
+    //     let driver = await new Builder().forBrowser("chrome").build();
 
-        let driver = await new Builder().forBrowser("chrome").build();
+    //     try {
+    //         await driver.get("https://lambdatest.github.io/sample-todo-app/");
+    //         await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
 
-        try {
-            await driver.get("https://lambdatest.github.io/sample-todo-app/");
-            await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
+    //         let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function (value) {
+    //             return value;
+    //         });
 
-            let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function (value) {
-                return value;
-            });
+    //         todoText.should.equal("Learn JavaSasdasdacript");  // Incorrect assertion
 
-            todoText.should.equal("Learn JavaSasdasdacript");  // Incorrect assertion
-
-            await driver.quit();
-        } catch (error) {
-            console.error("Error during test execution:", error);
-            throw error;  // Rethrow error to fail the test
-        }
-    });
+    //         await driver.quit();
+    //     } catch (error) {
+    //         console.error("Error during test execution:", error);
+    //         throw error;  // Rethrow error to fail the test
+    //     }
+    // });
 });
